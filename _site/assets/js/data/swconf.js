bd92b34837108c1f3a043e76906aead30d381b4e
Kuwguap <richierodney434@gmail.com>
2025-07-01 21:05:30 +0000

    My Pentesting AI Learned a New Word: Orchestrate.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751302175',resources: [
+    cacheName: 'chirpy-1751403887',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
